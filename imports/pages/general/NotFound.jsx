f2fa0e196ff9548f92f7d2fdd5bea504974e9124
Jérémy Simonklein <jeremy-s@equivote.fr>
2018-01-04 15:54:53 +0100

    Intégration de ReactRouter Création du component NotFound pour les pages non trouvées

---
+++ imports/pages/general/NotFound.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+
+    render() {
+        return (
+            <h1>Page non trouvée</h1>
+        );
+    }
+}
--- imports/startup/App.jsx
+++ imports/startup/App.jsx
@@ -1,12 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 
 import Landing from '/imports/pages/general/Landing';
+import NotFound from '/imports/pages/general/NotFound';
 
-export default class App extends Component{
+export default class App extends Component {
 
-    render(){
-        return(
-            <Landing />
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route path="*" component={NotFound} />
+                </Switch>
+            </Router>
         );
     }
 }
