a016509ab38b8d2b806546fe9b184f84ef8e877b
Jérémy Simonklein <jeremy-s@equivote.fr>
2018-01-05 15:04:28 +0100

    Ajout des boutons de connexion déconnexion dans la Navbar

--- imports/components/navigation/Navbar.jsx
+++ imports/components/navigation/Navbar.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data'
 
-export default class Navbar extends Component {
+export class Navbar extends Component {
+
+    logout = () => {
+        Meteor.logout()
+    }
 
     render() {
-        const {admin} = this.props
-        const custom_classes = admin && "white-item" 
-        
+        const { admin, user } = this.props
+        const custom_classes = admin && "white-item"
+
         return (
-            <Menu secondary borderless style={{backgroundColor: admin && "#6582c5"}}>
+            <Menu secondary borderless style={{ backgroundColor: admin && "#6582c5" }}>
                 {admin &&
                     <Menu.Item className={custom_classes} >
                         ADMIN
@@ -25,7 +30,36 @@ export default class Navbar extends Component {
                         Admin pages
                     </Menu.Item>
                 </Link>
+
+
+                {user ?
+                    <Menu.Menu position='right'>
+                        <Menu.Item className={custom_classes}>
+                            <Icon name="user" size="big" /> {user.username}
+                        </Menu.Item>
+                        <Menu.Item onClick={this.logout} className={custom_classes}>
+                            Déconnexion
+                        </Menu.Item>
+                    </Menu.Menu>
+                    :
+                    <Menu.Menu position='right'>
+                        <Menu.Item position="right" className={custom_classes}>
+                            <Link to="/sign_in" >
+                                Connexion / Inscription
+                        </Link>
+                        </Menu.Item>
+                    </Menu.Menu>
+                }
+
+
             </Menu>
         )
     }
 }
+
+export default NavbarContainer = withTracker(() => {
+    const user = Meteor.user()
+    return {
+        user
+    }
+})(Navbar)
